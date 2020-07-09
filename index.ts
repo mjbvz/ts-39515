c05389526d3940a8843ff4824c7dd1630d823704
Matt Bierner <matb@microsoft.com>
2020-07-08 19:20:42 -0700

    init

---
+++ index.ts
@@ -0,0 +1,7 @@
+import {
+    a1,
+    a2
+} from "./src/a";
+
+
+console.log(a1, a2, a3, a4)
---
+++ src/a.ts
@@ -0,0 +1,4 @@
+export const a1 = 123;
+export const a2 = 123;
+export const a3 = 123;
+export const a4 = 123;
